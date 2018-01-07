a06b7f234119b39d885b5d0d697b21fd839008f9
renx777 <arenukaprasad@gmail.com>
2018-01-08 03:02:00 +0530

    added license and code of conduct files

--- myjs.js
+++ myjs.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
 
 
-
+// usernames of twitch users
     var usernames = ["freecodecamp", "Hiko", "Krepo", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "cretetion", "OgamingSC2", "ESL_SC2", "brunofin"];
-
+// array to store offline users and online users
     var offliners = [];
     var onliners = [];
 
     console.log(usernames.length)
+// go through username array and for each user,get their stream data and render it
     $.each(usernames, function (index, value) {
 
         $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/" + value + "?callback=?", function (x) {
@@ -19,7 +20,7 @@ $(document).ready(function () {
             var purl = x.logo;
             var idx = value;
             var ids = "s" + value;
-
+// if data is not available,user empty logo
             if (x.logo == null) {
                 purl = "https://goo.gl/bKCtSo";
             }
@@ -36,20 +37,18 @@ $(document).ready(function () {
                 var j;
                 if (c.status) {
                     j = "Account closed"
-                }
-                else if (c.stream == null && c.status != 422) {
+                } else if (c.stream == null && c.status != 422) {
                     j = "Offline"
-                }
-                else {
+                } else {
 
                     j = c.stream.game + ":" + c.stream.channel.status;
 
                 }
 
+        // if stream is null or undefined add class offliners else add onliners class so
                 if (c.stream == null || c.stream == undefined) {
                     $('.all').append('<div class="container">' + '<div class="offliners">' + '<div class="row">' + '<div class="col-md-2 col-sm-12 col-xs-12">' + '<img src=' + purl + '></img>' + '</div>' + '<div class="col-md-4 col-sm-12 col-xs-12 ">' + '<a  data-toggle="toottip" title="Click to go to twitch stream channel" href=' + x.url + '>' + x.display_name + '</a>' + '</div>' + '<div class="col-md-6 col-sm-12 col-xs-12 ">' + '<span id>' + j + '</span>' + '</div></div></div>')
-                }
-                else {
+                } else {
                     $('.all').prepend('<div class="container">' + '<div class="onliners">' + '<div class="row">' + '<div class="col-md-2 col-sm-12 col-xs-12">' + '<img src=' + purl + '></img>' + '</div>' + '<div class="col-md-4 col-sm-12 col-xs-12 ">' + '<a  data-toggle="toottip" title="Click to go to twitch stream channel" href=' + x.url + '>' + x.display_name + '</a>' + '</div>' + '<div class="col-md-6 col-sm-12 col-xs-12 ">' + '<span id>' + j + '</span>' + '</div></div></div>')
                 }
 
@@ -77,7 +76,7 @@ $(document).ready(function () {
 
 
 
-
+// show offline,online and all users
 
     $('#on').on('click', function () {
         $('.offliners').hide()
